496e478ab88b55dff521cf22a0e8d8685ac101e3
ctranel <ctranel@CTRANEL.agsource.verona.crinet>
2013-08-30 10:29:26 -0500

    UPDATE: Genetics super section

--- gsg_app/controllers/custom_report/land.php
+++ gsg_app/controllers/custom_report/land.php
@@ -1,4 +1,11 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/* this page was added to support the dynamic menu generation.  There
+ * is not a separate supersection/section for custom reports, so we
+ * automatically redirect to the next level.
+ * 
+ */
+
+
 class Land extends CI_Controller {
 	function __construct(){
 		parent::__construct();
---
+++ gsg_app/controllers/genetics/land.php
@@ -0,0 +1,15 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Land extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+	}
+	
+	function index(){
+		redirect(site_url('genetics/cow'));
+	}
+	
+	function log_page(){
+		echo $this->access_log_model->write_entry(); //19 is the page code for DM Login
+		exit;
+	}
+}
