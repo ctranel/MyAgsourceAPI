bfe06d6f567c71bf7833a1ea1eb094b986e625fe
ctranel <ctranel@CTRANEL.agsource.verona.crinet>
2013-08-30 11:12:01 -0500

    UPDATE: Changes to accomodate GSG views

--- gsg_app/views/tci_d/app_nav.php
+++
@@ -1,5 +0,0 @@
-		<!-- <li class="first"><?php echo anchor('report_card/display', 'View Report Card', 'class="teal_banner"'); ?></li> -->
-	<?php if($this->as_ion_auth->has_permission("View Access Log")): ?>
-		<li><?php echo anchor('access_log/display/access_time/DESC/screen/12', 'Access Log', 'class="teal_banner"'); ?></li>
-	<?php endif; ?>
-		
