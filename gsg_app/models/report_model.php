0e82a9754312881d7bee0369a2e75b988c43c972
ctranel <ctranel@CTRANEL.agsource.verona.crinet>
2013-09-19 13:22:36 -0500

    FIX: summary_date field added.

--- gsg_app/models/report_model.php
+++ gsg_app/models/report_model.php
@@ -435,6 +435,9 @@ class Report_model extends CI_Model {
 		if (($key = array_search('cycle_date', $arr_select_fields)) !== FALSE) {
 			$arr_select_fields[$key] = "FORMAT(" . $this->primary_table_name . ".cycle_date, 'MM-dd-yy', 'en-US') AS cycle_date";//MMM-dd-yy
 		}
+		if (($key = array_search('summary_date', $arr_select_fields)) !== FALSE) {
+			$arr_select_fields[$key] = "FORMAT(" . $this->primary_table_name . ".summary_date, 'MM-dd-yy', 'en-US') AS summary_date";//MMM-dd-yy
+		}
 		foreach($arr_select_fields as $k => $v){
 			if(!empty($this->arr_aggregates[$k])){
 				$new_name = strtolower($this->arr_aggregates[$k]) . '_' . $v;
