df52167bb2506492d189caa8e172cf9ced584c15
Kevin Marshall <kmarshall@agsource.com>
2013-09-20 11:53:49 -0500

    Added Fresh Cow List and UHM List Controllers

---
+++ gsg_app/controllers/summary_reports/fresh_cow_list.php
@@ -0,0 +1,35 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once APPPATH . 'controllers/report_parent.php';
+class Fresh_Cow_List extends parent_report {
+	function __construct(){
+//		$this->section_path = 'herd_summary'; //this should match the name of this file (minus ".php".  Also used as base for css and js file names and model directory name
+//		$this->primary_model = 'herd_summary_model';
+		parent::__construct();
+		/* Load the profile.php config file if it exists
+		$this->config->load('profiler', false, true);
+		if ($this->config->config['enable_profiler']) {
+			$this->output->enable_profiler(TRUE);
+		} */
+	}
+
+	 function index($block_in = NULL, $sort_by = NULL, $sort_order = NULL, $display_format = NULL){
+	 	redirect(site_url('summary_reports/fresh_cow_list/fcl_cows'));
+	 }
+	function fcl_cows($block_in = NULL, $sort_by = NULL, $sort_order = NULL, $display_format = NULL){
+	 	$this->product_name = 'Fresh Cow List TCI Cows';
+		parent::display($block_in, $display_format);
+	 }
+	function fcl_heifers($block_in = NULL, $sort_by = NULL, $sort_order = NULL, $display_format = NULL){
+	 	$this->product_name = 'Fresh Cow List Fresh Heifers';
+		parent::display($block_in, $display_format);
+	 }
+	function fcl_spec($block_in = NULL, $sort_by = NULL, $sort_order = NULL, $display_format = NULL){
+	 	$this->product_name = 'Fresh Cow List Special';
+		parent::display($block_in, $display_format);
+	 }
+	 /*
+	 * ajax_report: Called via AJAX to populate graphs
+	 * to add flexibility (any graph/table can be called from any page),
+	 * all block generation code has been moved to the report parent ajax_report function
+	 */
+}
