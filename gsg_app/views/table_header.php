df0694d2a3c370bd3a2b1aabffe07f7277403fd7
ctranel <ctranel@CTRANEL.agsource.verona.crinet>
2013-10-25 14:16:50 -0500

    please work

--- gsg_app/views/table_header.php
+++ gsg_app/views/table_header.php
@@ -34,7 +34,6 @@ foreach($structure as $row): ?>
 //					$extra = Array('onclick'=>"return submit_table_sort_link('$form_id', '$submit_url');");
 //				}
 //				elseif($form_id == 'report_criteria'){ //reports that use ajax
-
 					$submit_url = '#';
 					$extra = Array('onclick'=>"return load_table(null, 'table-canvas" . $report_count . "', $report_count, '". $th['field_name'] . "', '$link_sort_order', '$block');");
 //				}
