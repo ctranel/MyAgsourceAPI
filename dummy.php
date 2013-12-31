2f77a287b582ca095ce8bdda1bc25bc7f597d8a9
Kevin Marshall <kmarshall@agsource.com>
2013-12-31 09:50:54 -0600

    INITIAL COMMIT: test20131231 branch* Branch stems off master after creating stable code for myagsource.com at 5nines in the master branch.  Includes earlier test branch that contains Carol's changes to webuser and its roles and my Filters library changes. *May contain bug in Herd Summary - Reproduction block.

--- dummy.php
+++ dummy.php
@@ -12,5 +12,5 @@ if(isset($_GET['debugger_connect']) && $_GET['debugger_connect'] == 1) {
 	}
 }
 // sheila here
-//TESTING: 5nines git hook and feweb origin ref url - take 2
+//TESTING: Initial commit of new test20131231 branch after first successful push to 5nines 
 ?>
