1fb3ff80e4995ea79434844b8a2cb5d193999a89
ctranel <tranelch@gmail.com>
2014-02-12 13:08:38 -0600

    UPDATE: Added user voice link

--- gsg_app/views/page_footer.php
+++ gsg_app/views/page_footer.php
@@ -37,6 +37,25 @@
 			 else window.onload = downloadJSAtOnload;
 		</script>	<?php
 	endif; ?>
-	
+	<!-- UserVoice JavaScript SDK (only needed once on a page) -->
+	<script>(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/i0OA99pO0xLAAKRentx5A.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()</script>
+
+	<!-- A tab to launch the Classic Widget -->
+	<script>
+		UserVoice = window.UserVoice || [];
+		UserVoice.push(['showTab', 'classic_widget', {
+		  mode: 'full',
+		  primary_color: '#006d73',
+		  link_color: '#006d73',
+		  default_mode: 'support',
+		  forum_id: 240012,
+		  support_tab_name: 'Report a bug',
+		  feedback_tab_name: 'Request a new feature',
+		  tab_label: 'Features & Bugs',
+		  tab_color: '#006d73',
+		  tab_position: 'bottom-right',
+		  tab_inverted: false
+		}]);
+	</script>
 </body>
 </html>
