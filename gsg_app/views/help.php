745f7207795bd82602c12fc0b3e30682b39b1c8e
Kevin Marshall <kmarshall@agsource.com>
2014-02-13 14:10:16 -0600

    TESTING: help view conditional

--- gsg_app/views/help.php
+++ gsg_app/views/help.php
@@ -4,7 +4,10 @@
 
 	$filepath = APPPATH.'helpdocs'.FS_SEP.$product_name.'.html';
 
+	echo $filepath;
+	
 	if (file_exists($filepath)) {
+		echo $filepath;
 		ob_start();
 		include $filepath;
 		echo ob_get_clean();
