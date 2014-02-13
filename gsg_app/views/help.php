bbbd2eebde3f92a5bf035349f4b403893f44ff17
Kevin Marshall <kmarshall@agsource.com>
2014-02-13 14:15:00 -0600

    FIXED: filename case in help view

--- gsg_app/views/help.php
+++ gsg_app/views/help.php
@@ -2,12 +2,9 @@
 
 	echo $page_header;
 
-	$filepath = APPPATH.'helpdocs'.FS_SEP.$product_name.'.html';
+	$filepath = APPPATH.'helpdocs'.FS_SEP.strtolower($product_name).'.html';
 
-	echo $filepath;
-	
 	if (file_exists($filepath)) {
-		echo $filepath;
 		ob_start();
 		include $filepath;
 		echo ob_get_clean();
