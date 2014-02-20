12a5e5b5544b0bcca913d529bc9d6175fe32ec2e
ctranel <tranelch@gmail.com>
2014-02-20 12:17:35 -0600

    UPDATE: Added default sort for lactation and offspring tables

--- gsg_app/models/cow_lookup/lactations_model.php
+++ gsg_app/models/cow_lookup/lactations_model.php
@@ -22,6 +22,7 @@ class Lactations_model extends CI_Model {
 		->select("lact_num, age, calving_date AS fresh_date, ltd_dim, ltd_milk_lbs, ltd_fat_lbs, ltd_pro_lbs, first_bred_dim, days_open, calving_int_days, avg_linear_score, d305_milk_lbs, d305_fat_lbs, d305_pro_lbs, me_milk_lbs, me_fat_lbs, me_pro_lbs, d365_milk_lbs, d365_fat_lbs, d365_pro_lbs, letter_grade_milk, letter_grade_fat, letter_grade_pro")
 		->where('herd_code', $herd_code)
 		->where('serial_num', $serial_num)
+		->order_by('lact_num', 'asc')
 		->get('vma.dbo.vma_Cow_Lookup_Lactations')
 		->result_array();
 		
@@ -36,6 +37,7 @@ class Lactations_model extends CI_Model {
 		->select("calf_control_num, calving_date, calf_name, calf_visible_id, sex_desc, twin_code, calving_ease_code, calf_sire_naab, calf_sire_name")
 		->where('herd_code', $herd_code)
 		->where('serial_num', $serial_num)
+		->order_by('calving_date', 'asc')
 		->get('vma.dbo.vma_Cow_Lookup_Calving')
 		->result_array();
 		
