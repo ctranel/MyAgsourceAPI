e582f0a89e10003f5b0aa4c674643a13816e1fcc
Kevin Marshall <kmarshall@agsource.com>
2014-02-26 10:49:54 -0600

    UPDATE: Clarified feedback instructions for the page not found view.

--- gsg_app/views/http_error.php
+++ gsg_app/views/http_error.php
@@ -8,9 +8,10 @@ window.onload=function(){
 if(isset($page_header)) echo $page_header;
 echo 'The address <strong>http://www.myagsource.com'.$_SERVER['REQUEST_URI'].'</strong> is invalid.<br /><br />';
 
-echo 'If you arrived here from a link on our site, please <a id="contact_us" href="mailto:support@myagsource.com">contact us</a>.<br /><br />';
+echo 'If you arrived here from a link on our site, please <a id="contact_us" href="mailto:support@myagsource.com">contact us</a>, 
+		describe where the link was, and check the box "Include a screenshot of this page" on the feedback window.<br /><br />';
 
 echo '-OR-<br /><br />';
 
-echo 'Email us at <a href="mailto:support@myagsource.com">support@myagsource.com</a>.';
+echo 'Email us at <a href="mailto:support@myagsource.com">support@myagsource.com</a><br /><br />.';
 if(isset($page_footer)) echo $page_footer;
