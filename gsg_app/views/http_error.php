1340d53f188a49863487e1a0299d60dd2e6f46d9
Kevin Marshall <kmarshall@agsource.com>
2014-02-26 10:09:31 -0600

    FIXED: Anchor to uservoice in http_error view.

--- gsg_app/views/http_error.php
+++ gsg_app/views/http_error.php
@@ -2,7 +2,7 @@
 if(isset($page_header)) echo $page_header;
 echo 'The address <strong>http://www.myagsource.com'.$_SERVER['REQUEST_URI'].'</strong> is invalid.<br /><br />';
 
-echo 'If you arrived here from a link on our site, please visit our <a href="myagsource.uservoice.com">UserVoice Feedback</a> page<br /><br />';
+echo 'If you arrived here from a link on our site, please visit our <a href="http://myagsource.uservoice.com">UserVoice Feedback</a> page<br /><br />';
 
 echo '-OR-<br /><br />';
 
