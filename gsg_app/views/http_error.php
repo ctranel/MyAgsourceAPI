7b1584d0b7150e23bc49c02550288a45d4f9c7b4
Kevin Marshall <kmarshall@agsource.com>
2014-02-26 10:31:25 -0600

    FIXED: Brought http_error functionality in line with Chris's Persnicketiness (contact us is now a uservoice popup dialog)

--- gsg_app/views/http_error.php
+++ gsg_app/views/http_error.php
@@ -1,10 +1,16 @@
+<script type="text/javascript">
+window.onload=function(){
+	UserVoice.push(['addTrigger', '#contact_us', {}]);
+}
+</script>
 <?php
+
 if(isset($page_header)) echo $page_header;
 echo 'The address <strong>http://www.myagsource.com'.$_SERVER['REQUEST_URI'].'</strong> is invalid.<br /><br />';
 
-echo 'If you arrived here from a link on our site, please visit our <a href="http://myagsource.uservoice.com">UserVoice Feedback</a> page<br /><br />';
+echo 'If you arrived here from a link on our site, please <a id="contact_us" href="mailto:support@myagsource.com">contact us</a>.<br /><br />';
 
 echo '-OR-<br /><br />';
 
-echo 'email us at <a href="mailto:support@myagsource.com">support@myagsource.com</a>.';
+echo 'Email us at <a href="mailto:support@myagsource.com">support@myagsource.com</a>.';
 if(isset($page_footer)) echo $page_footer;
