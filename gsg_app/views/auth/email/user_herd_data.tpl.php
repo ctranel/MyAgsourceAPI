51abf2931c2f11cfb59e4e40afc22ced6b597a2d
ctranel <tranelch@gmail.com>
2014-04-02 16:03:39 -0500

    UPDATE: Added registration name to herd info email, removed spacing between items.

--- gsg_app/views/auth/email/user_herd_data.tpl.php
+++ gsg_app/views/auth/email/user_herd_data.tpl.php
@@ -4,20 +4,21 @@
 	<div style="color: #D3D09E; background-color:#004147; border:solid 5px #004147; font-weight:bold;"><?php echo $this->config->item('product_name'); ?> - <?php echo $this->config->item('cust_serv_company'); ?></div>
 	<h1 style="font-size: 1.1em; background-color: #fff; color:#004147;">New <?php echo $this->config->item('product_name'); ?> Account Information</h1>
 	<h2>Registration Information</h2>
-	<p>Herd: <?php echo $herd_code; ?></p>
-	<p>Email: <?php echo $email; ?></p>
-	<p>Phone: <?php echo $phone; ?></p>
-	<p>Best time to call: <?php echo $best_time; ?></p>
+	<div>Name: <?php echo $first_name . ' ' . $last_name; ?></div>
+	<div>Herd: <?php echo $herd_code; ?></div>
+	<div>Email: <?php echo $email; ?></div>
+	<div>Phone: <?php echo $phone; ?></div>
+	<div>Best time to call: <?php echo $best_time; ?></div>
 	<h2>Herd Information</h2>
-	<p>Owner: <?php echo $arr_herd['herd_owner']; ?></p>
-	<p>Farm Name: <?php echo $arr_herd['farm_name']; ?></p>
-	<p>Address: <?php echo $arr_herd['address_1'] . '<br>' . $arr_herd['address_2'] . '<br>' . $arr_herd['city'] . ', ' . $arr_herd['state'] . ' ' . $arr_herd['zip_5']; ?></p>
-	<p>Contact: <?php echo $arr_herd['contact_fn'] . ' ' . $arr_herd['contact_ln']; ?></p>
-	<p>Primary Phone: <?php echo $arr_herd['primary_area_code'] . '-' . $arr_herd['primary_phone_num']; ?></p>
+	<div>Owner: <?php echo $arr_herd['herd_owner']; ?></div>
+	<div>Farm Name: <?php echo $arr_herd['farm_name']; ?></div>
+	<div>Address: <?php echo $arr_herd['address_1'] . '<br>' . $arr_herd['address_2'] . '<br>' . $arr_herd['city'] . ', ' . $arr_herd['state'] . ' ' . $arr_herd['zip_5']; ?></div>
+	<div>Contact: <?php echo $arr_herd['contact_fn'] . ' ' . $arr_herd['contact_ln']; ?></div>
+	<div>Primary Phone: <?php echo $arr_herd['primary_area_code'] . '-' . $arr_herd['primary_phone_num']; ?></div>
 	<h2>Tech Information</h2>
-	<p>Name: <?php echo $arr_tech['first_name'] . ' ' . $arr_tech['last_name']; ?></p>
-	<p>Home Phone: <?php echo $arr_tech['home_phone']; ?></p>
-	<p>Cell Phone: <?php echo $arr_tech['cell_phone']; ?></p>
-	<p>Association: <?php echo $arr_tech['association_num']; ?></p>
+	<div>Name: <?php echo $arr_tech['first_name'] . ' ' . $arr_tech['last_name']; ?></div>
+	<div>Home Phone: <?php echo $arr_tech['home_phone']; ?></div>
+	<div>Cell Phone: <?php echo $arr_tech['cell_phone']; ?></div>
+	<div>Association: <?php echo $arr_tech['association_num']; ?></div>
 	</body>
 </html>
