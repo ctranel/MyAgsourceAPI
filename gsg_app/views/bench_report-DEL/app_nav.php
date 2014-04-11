ba19244e37f77d53779cfeacb92876eab3cd6aca
ctranel <tranelch@gmail.com>
2014-04-11 16:33:19 -0500

    UPDATE: removed unused files

--- gsg_app/views/bench_report-DEL/app_nav.php
+++
@@ -1,4 +0,0 @@
-		<li class="first"><?php echo anchor('bench_report/download_log/display', 'Download Log', 'class="teal_banner"'); ?></li>
-	<?php if($this->as_ion_auth->has_permission("View Access Log")): ?>
-		<li><?php echo anchor('access_log/display/access_time/DESC/screen/2', 'Access Log', 'class="teal_banner"'); ?></li>
-	<?php endif; ?>
