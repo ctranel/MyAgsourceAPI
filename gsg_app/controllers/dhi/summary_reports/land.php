61976892e8e36d5d63a8997480edba2a2564211d
Kevin Marshall <kmarshall@agsource.com>
2014-04-16 11:46:59 -0500

    FIXED: Removed debug remnant die('here') in land controller for summary reports.

--- gsg_app/controllers/dhi/summary_reports/land.php
+++ gsg_app/controllers/dhi/summary_reports/land.php
@@ -1,7 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Land extends CI_Controller {
 	function __construct(){
-die('here');
 		parent::__construct();
 	}
 	
