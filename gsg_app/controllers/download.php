de78c7e9e9690175d3a5eba38615b2b9b3f7dff7
ctranel <tranelch@gmail.com>
2014-04-18 08:56:30 -0500

    UPDATE: Set up file download controller and added benchmark pdf links to dashboard

---
+++ gsg_app/controllers/download.php
@@ -0,0 +1,46 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Download extends CI_Controller {
+	
+	function __construct(){
+		parent::__construct();
+		if(!isset($this->as_ion_auth)){
+			redirect('auth/login', 'refresh');
+		}
+		if((!$this->as_ion_auth->logged_in())){
+			$redirect_url = set_redirect_url($this->uri->uri_string(), $this->session->flashdata('redirect_url'), $this->as_ion_auth->referrer);
+			$this->session->set_flashdata('redirect_url', $redirect_url);
+			if(strpos($this->session->flashdata('message'), 'Please log in.') === FALSE){
+				$this->session->set_flashdata('message',  $this->session->flashdata('message') . 'Please log in.');
+			}
+			else{
+				$this->session->keep_flashdata('message');
+			}
+			redirect(site_url('auth/login'));
+		}
+	}
+
+	function bench_all(){
+
+	}
+
+/**
+ * @method index()
+ * 
+ * @description Downloads the file passed in the parameter.  Must include "index" when creating link (e.g., .../download/index/filename.ext).  This file is not used when downloading dynamic data.
+ * 
+ * @param string filename to be downloaded from img directory
+ * @access	public
+ * @return	void
+ */
+	function index($filename){
+		$this->session->keep_flashdata('redirect_url');
+		if(file_exists(PROJ_DIR . FS_SEP . "img" . FS_SEP . $filename)){
+			$this->load->helper('download');
+			$data = file_get_contents(PROJ_DIR . FS_SEP . "img" . FS_SEP . $filename); // Read the file's contents
+			
+			force_download($filename, $data);
+		}
+		else show_404();//echo "cannot find file " . PROJ_DIR . FS_SEP . "img" . FS_SEP . $filename;
+		exit;
+	}
+}
--- gsg_app/views/auth/dashboard/resources.php
+++ gsg_app/views/auth/dashboard/resources.php
@@ -1,7 +1,7 @@
 <div class="widget-content">
 		<ul>
-			<li><?php echo anchor('img/bench-all.pdf', 'All Breed Benchmarks', array('id' => 'all-benchmarks')); ?></li>
-			<li><?php echo anchor('img/bench-ho.pdf', 'Holstein Benchmarks', array('id' => 'ho-benchmarks')); ?></li>
+			<li><?php echo anchor(site_url('download/index/bench_all.pdf'), 'All Breed Benchmarks', array('id' => 'all-benchmarks')); ?></li>
+			<li><?php echo anchor(site_url('download/index/bench_ho.pdf'), 'Holstein Benchmarks', array('id' => 'ho-benchmarks')); ?></li>
 		</ul>
 		<?php if(isset($inner_html)) echo $inner_html; ?>
 </div>
