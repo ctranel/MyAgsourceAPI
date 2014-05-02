11adcd2502dfa3ea6f7b8aa26542fd470ff7560c
Kevin Marshall <kmarshall@agsource.com>
2014-05-02 16:09:52 -0500

    FIXED: Changed Fresh Cow Number Sold or Died model to populate aggregates properly.

--- gsg_app/models/dhi/summary_reports/fresh_cow_summary/numbersoldordied_model.php
+++ gsg_app/models/dhi/summary_reports/fresh_cow_summary/numbersoldordied_model.php
@@ -5,6 +5,33 @@ class Numbersoldordied_model extends Report_model {
 		parent::__construct($section_path);
 	}
 	
+	//Overriding parent function to allow for a percentage row that includes values that can't be made by simply calculating the average (current month is incomplete and so is excluded in average)
+		
+	function get_fieldlist_array(){
+		$arr_total = array('Total'=>'total');
+		$arr_percent = array('Percent'=>'percent');
+		$this->arr_db_field_list = array_merge($this->arr_db_field_list,$arr_total,$arr_percent);
+		return $this->arr_db_field_list;
+	}
+	
+	//Overriding parent function to add total and percent columns
+	function get_table_header_data(){
+		$this->load->library('table_header');
+		$this->arr_unsortable_columns[] = 'Total';
+		$this->arr_unsortable_columns[] = 'Percent';
+		$this->arr_fields['Total'] = 'total';
+		$this->arr_fields['Percent'] = 'percent';
+		$table_header_data = array(
+				'arr_unsortable_columns' => $this->arr_unsortable_columns,
+				'arr_field_sort' => $this->arr_field_sort,
+				'arr_header_data' => $this->arr_fields,
+		);
+		$table_header_data['structure'] = $this->table_header->get_table_header_array($table_header_data['arr_header_data']);
+		$table_header_data['num_columns'] = $this->table_header->get_column_count();
+		return $table_header_data;
+	}
+	
+	
 	/*  
 	 * @method pivot() overrides report_model
 	 * @param array dataset
@@ -16,26 +43,31 @@ class Numbersoldordied_model extends Report_model {
 	 * @author ctranel
 	 */
 	public function pivot($arr_dataset, $header_field, $header_field_width, $label_column_width, $bool_avg_column = FALSE, $bool_sum_column = FALSE, $bool_bench_column = FALSE){
-		$avg_l1_sold_60_dim_cnt = $arr_dataset[0]['l1_sold_60_dim_cnt'];
-		$avg_l1_died_60_dim_cnt = $arr_dataset[0]['l1_died_60_dim_cnt'];
-		$avg_l4_sold_60_dim_cnt = $arr_dataset[0]['l4_sold_60_dim_cnt'];
-		$avg_l4_died_60_dim_cnt = $arr_dataset[0]['l4_died_60_dim_cnt'];
+		$l1_sold_cnt = $arr_dataset[0]['l1_sold_60_dim_cnt'];
+		$l1_died_cnt = $arr_dataset[0]['l1_died_60_dim_cnt'];
+		$l4_sold_cnt = $arr_dataset[0]['l4_sold_60_dim_cnt'];
+		$l4_died_cnt = $arr_dataset[0]['l4_died_60_dim_cnt'];
+		$l1_sold_pct = $arr_dataset[0]['l1_left_60_dim_pct'];
+		$l1_died_pct = $arr_dataset[0]['l1_died_60_dim_pct'];
+		$l4_sold_pct = $arr_dataset[0]['l4_left_60_dim_pct'];
+		$l4_died_pct = $arr_dataset[0]['l4_died_60_dim_pct'];
+		
+		
 		$new_dataset = parent::pivot($arr_dataset, $header_field, $header_field_width, $label_column_width, $bool_avg_column, $bool_sum_column, $bool_bench_column);
 		//update total field in new dataset
-		$new_dataset['l1_sold_60_dim_cnt']['total'] = $avg_l1_sold_60_dim_cnt;
-		$new_dataset['l1_died_60_dim_cnt']['total'] = $avg_l1_died_60_dim_cnt;
-		$new_dataset['l4_sold_60_dim_cnt']['total'] = $avg_l4_sold_60_dim_cnt;
-		$new_dataset['l4_died_60_dim_cnt']['total'] = $avg_l4_died_60_dim_cnt;
-		$new_dataset['l1_sold_60_dim_cnt']['average'] = round($avg_l1_sold_60_dim_cnt / 12);
-		$new_dataset['l1_died_60_dim_cnt']['average'] = round($avg_l1_died_60_dim_cnt / 12);
-		$new_dataset['l4_sold_60_dim_cnt']['average'] = round($avg_l4_sold_60_dim_cnt / 12);
-		$new_dataset['l4_died_60_dim_cnt']['average'] = round($avg_l4_died_60_dim_cnt / 12);
-		//Change Header Text
-		$this->arr_fields['Total'] = 'average';
-		unset($this->arr_fields['Average']);
-		$this->arr_fields['Pct'] = 'total';
-		unset($this->arr_fields['Total']);
-		
+		unset($new_dataset['l1_left_60_dim_pct']);
+		unset($new_dataset['l4_left_60_dim_pct']);
+		unset($new_dataset['l1_died_60_dim_pct']);
+		unset($new_dataset['l4_died_60_dim_pct']);
+		$new_dataset['l1_sold_60_dim_cnt']['total'] = $l1_sold_cnt;
+		$new_dataset['l1_died_60_dim_cnt']['total'] = $l1_died_cnt;
+		$new_dataset['l4_sold_60_dim_cnt']['total'] = $l4_sold_cnt;
+		$new_dataset['l4_died_60_dim_cnt']['total'] = $l4_died_cnt;
+		$new_dataset['l1_sold_60_dim_cnt']['percent'] = $l1_sold_pct;
+		$new_dataset['l1_died_60_dim_cnt']['percent'] = $l1_died_pct;
+		$new_dataset['l4_sold_60_dim_cnt']['percent'] = $l4_sold_pct;
+		$new_dataset['l4_died_60_dim_cnt']['percent'] = $l4_died_pct;
+
 		return $new_dataset;
 	}
 
