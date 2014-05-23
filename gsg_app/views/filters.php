f8454637001b0aa86a48f6eb17dca56ab3b92614
Kevin Marshall <kmarshall@agsource.com>
2014-05-23 13:05:32 -0500

    UPDATE: Added test_date filters

--- gsg_app/views/filters.php
+++ gsg_app/views/filters.php
@@ -150,6 +150,12 @@
 					Recheck&nbsp;&nbsp;</span>
 					<?php echo form_fieldset_close();
 				break;
+			case 'test_date':
+				echo form_fieldset('Test Date Range', array('id' => 'test_date-fieldset')); ?>
+					Between <?php echo form_input(array('name'=>'test_date_dbfrom', 'value'=>$filter_selected['test_date_dbfrom'], 'size'=>'10','maxlength'=>'10', 'id'=>'datepickfrom')) ?>
+					and <?php echo form_input(array('name'=>'test_date_dbto', 'value'=>$filter_selected['test_date_dbto'], 'size'=>'10', 'maxlength'=>'10', 'id'=>'datepickto')); ?>
+				<?php echo form_fieldset_close();
+				break;
 		endswitch; 
 	endforeach;
 endif; ?>
