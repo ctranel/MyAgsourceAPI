87346accc13427b3a525fdb09079fa527e8079de
ctranel <tranelch@gmail.com>
2014-07-16 18:10:18 -0500

    BUGFIX: Fixed error when there is a section with no pages

--- gsg_app/views/section_nav.php
+++ gsg_app/views/section_nav.php
@@ -1,12 +1,16 @@
 <?php 	$class = 'first';
-		if(!isset($section_id)) $section_id = '';
-		foreach($arr_pages as $p): 
-			if($section_path == $p['path']):
-				$class .= ' current';
-			endif; ?>
-			<li class = "<?php echo $class; ?>"><?php echo anchor($p['path'], $p['name'], 'class="section-nav"'); ?></li>
-<?php 		$class = '';
-		endforeach;
+		if(!isset($section_id)){
+			$section_id = '';
+		}
+		if(isset($arr_pages) && is_array($arr_pages)){
+			foreach($arr_pages as $p): 
+				if($section_path == $p['path']):
+					$class .= ' current';
+				endif; ?>
+				<li class = "<?php echo $class; ?>"><?php echo anchor($p['path'], $p['name'], 'class="section-nav"'); ?></li>
+<?php	 		$class = '';
+			endforeach;
+		}
 		if($this->as_ion_auth->has_permission("View Access Log") && FALSE): ?>
 			<li class = "<?php echo $class; ?>"><?php echo anchor('access_log/display/access_time/DESC/screen/' . $section_id, 'Access Log', 'class="section-nav"'); ?></li>
 <?php		$class = '';
