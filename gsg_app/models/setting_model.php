1e83f2285cc76c7638de29bb7ab1f469f96520af
ctranel <tranelch@gmail.com>
2014-07-30 17:14:03 -0500

    BUGFIX: Fixed consequenses of sample herd login.

--- gsg_app/models/setting_model.php
+++ gsg_app/models/setting_model.php
@@ -26,22 +26,23 @@ class Setting_model extends CI_Model {
 	 * @author ctranel
 	 **/
 	public function getSettingsByCategory($category, $user_id, $herd_code) {
-		$this->db
-			->select('s.id, t.name AS type, c.name AS category, g.name AS [group], s.name, s.description, s.default_value, uhs.value')
-			//->join('users.dbo.set_user_herd_settings uhs', "s.id = uhs.setting_id AND uhs.user_id = " . $user_id . " AND uhs.herd_code = '" . $herd_code . "'", 'left')
-			->join('users.dbo.set_lookup_types t', "s.type_id = t.id", 'inner')
-			->join('users.dbo.set_lookup_categories c', "s.category_id = c.id", 'inner')
-			->join('users.dbo.set_lookup_groups g', "s.group_id = g.id", 'inner')
-			->where('c.name', $category);
-		if($user_id){
-			$this->db->where("uhs.user_id = " . $user_id . "")
-				->join('users.dbo.set_user_herd_settings uhs', "s.id = uhs.setting_id AND uhs.user_id = " . $user_id . " AND uhs.herd_code = '" . $herd_code . "'", 'left');
+		if(isset($user_id) && $user_id !== FALSE){
+			$this->db
+				->join('users.dbo.set_user_herd_settings uhs', "s.id = uhs.setting_id AND uhs.user_id = " . $user_id . " AND uhs.herd_code = '" . $herd_code . "'", 'left')
+				->where("(uhs.user_id = " . $user_id . " OR uhs.user_id IS NULL)");
 		}
 		else{
-			$this->db->where("uhs.user_id IS NULL")
-				->join('users.dbo.set_user_herd_settings uhs', "s.id = uhs.setting_id AND uhs.user_id IS NULL AND uhs.herd_code = '" . $herd_code . "'", 'left');
+			$this->db
+				->join('users.dbo.set_user_herd_settings uhs', "s.id = uhs.setting_id AND uhs.user_id IS NULL AND uhs.herd_code = '" . $herd_code . "'", 'left')
+				->where("uhs.user_id IS NULL");
 		}
-		$results = $this->db->where("(uhs.herd_code = '" . $herd_code . "' OR uhs.herd_code IS NULL)")
+		$results = $this->db
+		->select('s.id, t.name AS type, c.name AS category, g.name AS [group], s.name, s.description, s.default_value, uhs.value')
+		->join('users.dbo.set_lookup_types t', "s.type_id = t.id", 'inner')
+		->join('users.dbo.set_lookup_categories c', "s.category_id = c.id", 'inner')
+		->join('users.dbo.set_lookup_groups g', "s.group_id = g.id", 'inner')
+		->where('c.name', $category)
+		->where("(uhs.herd_code = '" . $herd_code . "' OR uhs.herd_code IS NULL)")
 		->get('users.dbo.set_settings s')
 		->result_array();
 		
