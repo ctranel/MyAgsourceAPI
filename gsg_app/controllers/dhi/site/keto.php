3d2d3240b7ef8b7e2041da20491d955342707855
ctranel <tranelch@gmail.com>
2015-01-12 11:49:28 -0600

    BUGFIX: Fixed url

--- gsg_app/controllers/dhi/site/keto.php
+++ gsg_app/controllers/dhi/site/keto.php
@@ -15,7 +15,7 @@ class Keto extends parent_report {
 	function index($block_in = NULL, $display_format = NULL, $sort_by = NULL, $sort_order = NULL){
 		$redirect_url = set_redirect_url($this->uri->uri_string(), $this->session->flashdata('redirect_url'), $this->as_ion_auth->referrer);
 		$this->session->set_flashdata('redirect_url', $redirect_url);
-		redirect(site_url('dhi/site/prototypes/keto_summary'));
+		redirect(site_url('dhi/site/keto/keto_summary'));
 	}
 	function keto_summary($block_in = NULL, $display_format = NULL, $sort_by = NULL, $sort_order = NULL){
 	 	$this->product_name = 'Summary';
