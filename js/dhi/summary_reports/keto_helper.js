8498d94e460e956c0e263996b4eea1a959e98453
rhacer <kmarshall@agsource.com>
2015-01-27 13:12:16 -0600

    UPDATE: Added Keto_helper to format x-axis dates on graphs to be [month year] rather than a full date.

---
+++ js/dhi/summary_reports/keto_helper.js
@@ -0,0 +1,10 @@
+// this function allows you to change chart options immediately before chart is rendered
+	function pre_render(options_in){
+		//if there is a second yAxis, change the color
+		if(section_data['block'] == 'overall_keto_prev' ||
+			section_data['block'] == 'overall_early_prev_graph' 
+			) {	
+			options_in.xAxis[0].labels.formatter = function(){return Highcharts.dateFormat('%b %Y', this.value);};
+		}
+		return options_in;
+	}
