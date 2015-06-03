df18a2a137534bdde5b13fe61f3bcf04daced1e3
ctranel <tranelch@gmail.com>
2015-06-03 15:51:54 -0500

    UPDATE: Updated FPR for new codebase

--- gsg_app/libraries/dhi/summary_reports/fresh_cow_summary/fc_tci/cowpopulations.php
+++ gsg_app/libraries/dhi/summary_reports/fresh_cow_summary/fc_tci/cowpopulations.php
@@ -11,14 +11,11 @@ class Cowpopulations extends TableData {
 	/*  
 	 * @method pivot() overrides report_model
 	 * @param array dataset
-	 * @param string header field
-	 * @param int pdf with of header field
-	 * @param bool add average column
-	 * @param bool add sum column
 	 * @return array pivoted resultset
 	 * @author ctranel
 	 */
 	public function pivot($arr_dataset){
+		//Top row is summary data.  Pull relevant info and remove row
 		$l1_avg_calving_cnt = $arr_dataset[0]['l1_avg_calving_cnt'];
 		$l4_avg_calving_cnt = $arr_dataset[0]['l4_avg_calving_cnt'];
 		$l0_avg_calving_cnt = $arr_dataset[0]['l0_avg_calving_cnt'];
@@ -28,11 +25,13 @@ class Cowpopulations extends TableData {
 		unset($arr_dataset[0]);
 		
 		$new_dataset = parent::pivot($arr_dataset);
-		//update total field in new dataset
+
+		//Remove columns that were used only to pull summary data
 		unset($new_dataset['l1_avg_calving_cnt']);
 		unset($new_dataset['l4_avg_calving_cnt']);
 		unset($new_dataset['l0_avg_calving_cnt']);
 
+		//Insert summary data into dataset
 		$new_dataset['fresh_month'][] = 'Annual Average';
 		$new_dataset['l1_calving_cnt'][] = $l1_avg_calving_cnt;
 		$new_dataset['l4_calving_cnt'][] = $l4_avg_calving_cnt;
@@ -41,6 +40,7 @@ class Cowpopulations extends TableData {
 		$new_dataset['l1_calving_cnt'][] = $l1_tot_calving_cnt;
 		$new_dataset['l4_calving_cnt'][] = $l4_tot_calving_cnt;
 		$new_dataset['l0_calving_cnt'][] = $l0_tot_calving_cnt;
+
 		return $new_dataset;
 	}
 }
