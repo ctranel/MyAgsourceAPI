4df2bde2f9cad4148704b3164d7259e0f92caabc
rhacer <kmarshall@agsource.com>
2015-06-30 15:34:17 -0500

    FIXED: Graph colors coordinated between graphs and regression lines

--- js/dhi/summary_reports/fresh_cow_summary_helper.js
+++ js/dhi/summary_reports/fresh_cow_summary_helper.js
@@ -1,12 +1,5 @@
 	// this function allows you to change chart options immediately before chart is rendered
 	function pre_render(options_in){
-		//if there is a second yAxis, change the color
-		if(typeof options_in.yAxis[1] != 'undefined'){
-			options_in.yAxis[1].title.style.color = var_arr_graph_colors[1];
-		}
-		if(client_data['block'] == 'ratio_of_first_test_fat_t' || client_data['block'] == 'transition_cow_index_tci_' || client_data['block'] == 'first_test_linear_score_b'){
-			options_in.colors = [var_arr_graph_colors[0], var_arr_graph_colors[0], var_arr_graph_colors[1], var_arr_graph_colors[1]];
-		}
 
 		//if fpr, set yaxis tick interval to .2
 		if(client_data['block'] == 'ratio_of_first_test_fat_t'){
