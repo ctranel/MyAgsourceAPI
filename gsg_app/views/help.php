a5b1a324b49a8c73b37e74051a31db1fcc34a7d6
rhacer <kmarshall@agsource.com>
2015-07-06 14:56:08 -0500

    UPDATE: Removed significant amount of the Help page.

--- gsg_app/views/help.php
+++ gsg_app/views/help.php
@@ -18,56 +18,6 @@ endif; ?>
 				always email us.
 			</li>
 		</ul>
-	<h2>A Message for our users who have found an error</h2>
-		<p>We are doing our best to address every issue in a timely manner.
-		However, we are undoubtly going to miss at least one thing as that
-		is the nature of the programming business.</p>
-		<p>This is where you, our users, come in. Every new program needs 
-		feedback from its users.  Users always try things in ways the designers
-		and programmers couldn't possibly have predicted. This is why engaged users
-		are an essential and greatly appreciated part of every project's
-		community.  Be sure to make use of uservoice linked above so we and
-		the rest of the MyAgsource community hears from you.</p>
-		<p>Thank you for helping us make MyAgsource.com fulfill your
-		needs and the needs of our cooperative as a whole.</p>
-	<h2>Known Issues</h2>
-		<ul>
-			<li>Google Chrome
-				<p><label>Version 32</label> Chrome 32 contains a bug that affects our
-					scatter plot graphs. Google pushed the fix up to Chrome 33.  If you're
-					still using Chrome 32, please update to 33 or higher.
-				</p>
-			</li>
-			<li>Internet Explorer
-				<p><label>Version 7 and lower</label> IE 7 and lower are incompatible with
-					a code package essential to the site's ability to display cows
-					when clicking on their identification links. If you wish to be
-					able to use this feature on a windows machine running Windows XP,
-					consider using a different browser when visiting
-					myagsource.com.
-				</p>
-			</li>
-		</ul>
-	<h2>Recommended Browser</h2>
-		<p>While we are constantly addressing cross-browser compatibility
-			issues and recognize our members have preferred browsers across the
-			spectrum, we cannot promise 100% compatibility at all times with all
-			browsers. As such, we can only inform you of compatibility issues as
-			they arise and if/when they'll be addressed.</p>
-		<p> We encourage you to use your favorite browser as you normally would
-			and please inform us of any issues you encounter with the website 
-			either via the uservoice or email links provided above.</p>
-		<p> In the meantime, if you would like to view the website as it
-			is intended to look for comparison, the browsers most compatibile
-			with the site as of 04/06/2015 are:
-			<ul><li><strong>Firefox</strong></li>
-			<li><strong>Chrome 33 and above</strong></li></ul></p>
-		<p> We will never require you to change browsers if it is within
-			our power to fix an issue. However, some things are beyond our
-			control and those things will be listed above in our Known Issues
-			section. The browser(s) with the least known issues will be recommended
-			here.</p>
-		<p> Thanks for your understanding.</p>
 </div>
 <?php if(isset($page_footer)): 
 	echo $page_footer;
