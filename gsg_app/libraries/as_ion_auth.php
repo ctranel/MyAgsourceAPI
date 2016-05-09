448c3d3a9726e8002aae84d70fdcd83fabe08ba8
ctranel <ctranel@agsource.com>
2016-05-09 10:07:43 -0500

    UPDATE: changed method of sending emails, added my address for verification
    Had comma delimited list, now trying array.  Both should be valid.

--- gsg_app/libraries/as_ion_auth.php
+++ gsg_app/libraries/as_ion_auth.php
@@ -84,7 +84,7 @@ class As_ion_auth extends \Ion_auth {
         $message = $this->load->view($this->config->item('email_templates', 'ion_auth').$this->config->item('user_herd_data', 'ion_auth'), $data, true);
         $this->email->clear();
         $this->email->from($this->config->item('admin_email'), $this->config->item('site_title'));
-        $this->email->to($this->config->item('cust_serv_email'), $this->config->item('field_email'));
+        $this->email->to([$this->config->item('cust_serv_email'), $this->config->item('field_email'), 'ctranel@agsource.com']);
         $this->email->subject($this->config->item('site_title') . ' - Account Activation Info - ' . $additional_data['last_name']);
 //				$this->email->subject($this->config->item('site_title') . ' - Account Activation Info');
         $this->email->message($message);
