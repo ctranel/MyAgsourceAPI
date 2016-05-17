a9a2c6b46d4546877dc01e03e2a54590d54e48af
ctranel <ctranel@agsource.com>
2016-05-17 10:40:01 -0500

    BUGFIX: Fixed "gnomic" typo

--- gsg_app/views/auth/dashboard/message.php
+++ gsg_app/views/auth/dashboard/message.php
@@ -1,4 +1,4 @@
 <ul>
-    <li><a href="/dhi/reports/id/gen_verif">Genomic Verified Animals</a> under the ID-Inventory menu lists all the animals that have been gnomically tested.</li>
+    <li><a href="/dhi/reports/id/gen_verif">Genomic Verified Animals</a> under the ID-Inventory menu lists all the animals that have been genomically tested.</li>
     <li><a href="/dhi/reports/lab/bulk_scc">Bulk Tank SCC Contribution</a> under the Lab Results menu displays the current test Cow SCC data that was found on AgSource Alert.</li>
 </ul>
