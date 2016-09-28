0f24dea38b581be11905d63075b8b277a523185f
ctranel <ctranel@agsource.com>
2016-09-28 15:28:12 -0500

    UPDATE: keyed blocks with list order value

--- api/gsg_app/models/web_content/block_model.php
+++ api/gsg_app/models/web_content/block_model.php
@@ -18,7 +18,14 @@ class Block_model extends CI_Model {
 			->join('users.dbo.pages_blocks pb', 'b.id = pb.block_id', 'inner')
 			->order_by('list_order', 'asc')
 			->from('users.dbo.blocks b');
-		return $this->db->get()->result_array();
+		$results = $this->db->get()->result_array();
+        $ret = [];
+
+        array_walk($results, function($v, $k) use (&$ret){
+            $ret[$v['list_order']] = $v;
+        });
+
+        return $ret;
 	}
 	
 	/**
@@ -39,68 +46,4 @@ class Block_model extends CI_Model {
 		}
 		return $this->getBlocks();
 	}
-	
-	/**
-	 * get_block_links
-	 * @param int section id
-	 * @return array of block info keyed by path
-	 * @author ctranel
-	public function getCompleteData() {
-		$arr_return = [];
-		if(isset($section_id)) $this->db->where('p.section_id', $section_id);
-		$result = $this->db
-		->select("p.id AS page_id, b.id, p.section_id, b.path, b.name, ct.name AS chart_type, b.description, p.path AS page, p.name AS page_name, CASE WHEN dt.name LIKE '%chart' THEN 'chart' ELSE dt.name END AS display_type,s.path AS section_path, b.max_rows, b.cnt_row, b.sum_row, b.avg_row, b.bench_row, pf.db_field_name AS pivot_db_field, b.is_summary")
-		->join('users.dbo.pages AS p', 'p.section_id = s.id', 'left')
-		->join('users.dbo.pages_blocks AS pb', 'p.id = pb.page_id', 'left')
-		->join('users.dbo.blocks AS b', 'pb.block_id = b.id', 'left')
-		->join('users.dbo.lookup_display_types AS dt', 'b.display_type_id = dt.id', 'left')
-		->join('users.dbo.lookup_chart_types AS ct', 'b.chart_type_id = ct.id', 'left')
-		->join('users.dbo.db_fields AS pf', 'pf.id = b.pivot_db_field', 'left')
-		//->where($this->tables['blocks'] . '.display IS NOT NULL')
-		->where('b.path IS NOT NULL')
-		->order_by('s.list_order', 'asc')
-		->order_by('p.list_order', 'asc')
-		->order_by('pb.list_order', 'asc')
-		->get('users.dbo.sections AS s')->result_array();
-//@todo: remove code below from model file
-		if(is_array($result) && !empty($result)){
-			foreach($result as $r){
-				$arr_return[$r['page']]['page_id'] = $r['page_id'];
-				$arr_return[$r['page']]['name'] = $r['page_name'];
-				if(empty($r['path']) === FALSE){
-					$arr_return[$r['page']]['blocks'][$r['path']] = array(
-						'id'=>$r['id'],
-						'section_id'=>$r['section_id'],
-						'name'=>$r['name'],
-						'description'=>$r['description'],
-						'path'=>$r['path'],
-						'section_path'=>$r['section_path'],
-						'display_type'=>$r['display_type'],
-						'chart_type'=>$r['chart_type'],
-						'max_rows'=>$r['max_rows'],
-						'cnt_row'=>$r['cnt_row'],
-						'sum_row'=>$r['sum_row'],
-						'avg_row'=>$r['avg_row'],
-						'bench_row'=>$r['bench_row'],
-						'is_summary'=>$r['is_summary'],
-						'pivot_db_field'=>$r['pivot_db_field']
-					);
-				} 
-				else	{
-					$arr_return[$r['page']]['blocks'][$r['path']] = array(
-						'id'=>$r['id'],
-						'section_id'=>$r['section_id'],
-						'name'=>$r['name'],
-						'description'=>$r['description'],
-						'path'=>$r['path'],
-						'is_summary'=>$r['is_summary'],
-						'section_path'=>$r['section_path']
-					);
-				}
- 			}
- 			return $arr_return;
-		}
-		else return FALSE;
-	}
-**/
 }
