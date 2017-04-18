370ddbb24da79a0cb308274ecf8e10372c00949a
ctranel <ctranel@agsource.com>
2017-04-18 15:11:05 -0500

    BUGFIX: Fixed typo in field name

--- api/gsg_app/models/dhi/cow_lookup/lactations_model.php
+++ api/gsg_app/models/dhi/cow_lookup/lactations_model.php
@@ -19,12 +19,12 @@ class Lactations_model extends CI_Model {
 	
 	public function getLactationsArray($herd_code, $serial_num){
 		$arr_ret = $this->{$this->db_group_name}
-		->select("lact_num, age, calving_date AS fresh_date, ltd_dim, ltd_milk_lbs, ltd_fat_lbs, ltd_pro_lbs, first_bred_dim, days_open, calving_int_days, avg_linear_score, d305_milk_lbs, d305_fat_lbs, d305_pro_lbs, me_milk_lbs, me_fat_lbs, me_pro_lbs, d365_milk_lbs, d365_fat_lbs, d365_pro_lbs, letter_grade_milk, letter_grade_fat, letter_grade_pro")
-// 		->where('herd_code', $herd_code)
-// 		->where('serial_num', $serial_num)
+		->select("lact_num, age, calving_date AS fresh_date, ltd_dim, ltd_milk_lbs, ltd_fat_lbs, ltd_pro_lbs, first_bred_dim, days_open, calving_int_days, avg_linear_score, d305_milk_lbs, d305_fat_lbs, d305_pro_lbs, me_milk_lbs, me_fat_lbs, me_pro_lbs, d365_milk_lbs, d365_fat_lbs, d365_pro_lbs, milk_letter_grade, fat_letter_grade, pro_letter_grade")
+ 		->where('herd_code', $herd_code)
+ 		->where('serial_num', $serial_num)
 		->order_by('lact_num', 'asc')
-// 		->get('vma.dbo.vma_Cow_Lookup_Lactations')
-        ->get("vma.dbo.ufn_animal_lactation('".$herd_code."',".$serial_num.")")
+ 		->get('vamTD.dbo.vma_Cow_Lookup_Lactations')
+//        ->get("vma.dbo.ufn_animal_lactation('".$herd_code."',".$serial_num.")")
 		->result_array();
 		
 		if(is_array($arr_ret)){
